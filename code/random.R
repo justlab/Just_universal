42bf9492a04dbdcda8a42350480179150ce58ce6
Kodi Arfer <git@arfer.net>
2018-11-02 09:18:41 -0400

    make.seed.for.obj: Return integers as-is.

--- code/random.R
+++ code/random.R
@@ -3,7 +3,9 @@ set.seed.with.obj = function(x)
 
 make.seed.for.obj = function(x)
   # https://stackoverflow.com/a/52787092
-   {x = as.raw(as.hexmode(substring(
+   {if (is.integer(x))
+        return(x)
+    x = as.raw(as.hexmode(substring(
         digest::digest(x, algo = "xxhash32"),
         c(1, 3, 5, 7),
         c(2, 4, 6, 8))))
