85655b6e86f499051a3a651a6c15c5ba38ba1536
Kodi Arfer <git@arfer.net>
2019-05-13 11:19:11 -0400

    Added `punl.R`.

---
+++ code/punl.R
@@ -0,0 +1,17 @@
+punl = function(...)
+  # Like 'list', but uses punning to generate default names, so
+  #   punl(a, b, c = x, d)
+  # is equivalent to
+  #   list(a = a, b = b, c = x, d = d)
+  # As a bonus, 'punl()' is equivalent to
+  #   `names<-`(list(), character())
+  # That is, 'punl()' creates an empty named list.
+   {m = match.call(expand.dots = F)
+    inp = as.character(m$"...")
+    l = list(...)
+    names(l) =
+        (if (is.null(names(l)))
+            inp
+         else
+            ifelse(names(l) == "", inp, names(l)))
+    l}
