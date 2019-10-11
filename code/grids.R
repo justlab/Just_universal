1e90f06751fbe7f0a6067dad57662a34a8b092b3
Kodi Arfer <git@arfer.net>
2019-10-11 15:24:49 -0400

    [grids] Added `patchwork.regionalize`.

--- code/grids.R
+++ code/grids.R
@@ -25,3 +25,56 @@ square.xyd = function(x, y, d)
        x - d/2, y - d/2,    # SW
        x + d/2, y - d/2,    # SE
        x + d/2, y + d/2)))) # NE
+
+patchwork.regionalize = function(x, y, n.regions)
+  # Given cells in a square grid, provided by `x` and `y`
+  # vectors, split the area into `n.regions` regions that are
+  # roughly rectangular, roughly contiguous, and comprising equal
+  # numbers of cells (except cells left over after division by
+  # `n.regions`). Return a vector of the regions, numbered 1 through
+  # `n.regions`.
+   {suppressPackageStartupMessages(
+       {library(data.table)})
+
+    stopifnot(length(x) == length(y))
+    stopifnot(n.regions <= length(x))
+
+    mg = data.table(
+        x = 1 + round((x - min(x)) / median(diff(sort(unique(x))))),
+        y = 1 + round((y - min(y)) / median(diff(sort(unique(y))))))
+    stopifnot(nrow(unique(mg)) == nrow(mg))
+
+    OUT.OF.BOUNDS = -1
+    UNASSIGNED = 0
+
+    m = matrix(OUT.OF.BOUNDS, nrow = max(mg$x), ncol = max(mg$y))
+    m[mg[, cbind(x, y)]] = UNASSIGNED
+
+    x.max = nrow(m)
+    y.max = ncol(m)
+
+    x.start = min(mg$x)
+    y.start = mg[x == x.start, min(y)]
+
+    lapply(1 : n.regions, function(region)
+       {to.add = nrow(mg) %/% n.regions +
+            (region <= nrow(mg) %% n.regions)
+        added = 0
+        for (d in 0 : max(x.max, y.max))
+           {for (dx in -d : d)
+               {x = x.start + dx
+                if (x < 1 || x > x.max)
+                    next
+                for (y in y.start +
+                         (if (abs(dx) == d) (-d : d) else c(-d, d)))
+                    {if (y < 1 || y > y.max)
+                         next
+                     if (m[x, y] == UNASSIGNED)
+                        {if (added == to.add)
+                            {x.start <<- x
+                             y.start <<- y
+                             return()}
+                         m[x, y] <<- region
+                         added = added + 1}}}}})
+
+    m[mg[, cbind(x, y)]]}
