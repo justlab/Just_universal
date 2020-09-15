3e57080a120395d9f75133d7df739c434dbf281d
Kodi Arfer <git@arfer.net>
2020-09-15 08:26:03 -0400

    [download] Use a database instead of JSON for speed.

--- R/download.R
+++ R/download.R
@@ -1,34 +1,45 @@
 #' @export
+#' @import DBI
 download.update.meta = function(
         url, dir, to = basename(url), curl = character())
   # Download a file from `url` and save it to `to.path` = `dir`/`to`
   # if there isn't already a file there, creating any needed
-  # directories. Then create or update a metadata file in `dir` that
-  # shows where each file was gotten, when it was downloaded, and its
-  # hash. Return `to.path`.
+  # directories. Then create or update a database of metadata in `dir`
+  # that shows where each file was gotten, when it was downloaded, and
+  # its hash. Return `to.path`.
    {to.path = file.path(dir, to)
     dir.create(dirname(to.path), showWarnings = F, recursive = T)
 
     if (!file.exists(to.path))
-       {meta.path = file.path(dir, "meta.json")
-        meta = (if (!file.exists(meta.path)) list() else
-            jsonlite::fromJSON(simplifyVector = F, meta.path))
-
-        stopifnot(0 == system2("curl", shQuote(c(
+       {stopifnot(0 == system2("curl", shQuote(c(
             url, "-o", to.path,
             "--fail", "--remote-time",
             "--user-agent", "some-program",
             curl))))
 
-        meta[[to]] = list(
+        meta = dbConnect(RSQLite::SQLite(), file.path(dir, "meta.sqlite"))
+        on.exit(dbDisconnect(meta))
+        if (!as.integer(dbGetQuery(meta,
+                "select count(*) from sqlite_master where type = 'table'")))
+           {dbExecute(meta, "pragma journal_mode = wal")
+            dbExecute(meta, "create table Downloads
+               (file             text primary key,
+                url              text,
+                time_downloaded  integer,
+                sha256           blob not null) without rowid")}
+        append.one.row.to.db(meta, "Downloads", list(
+            file = to,
             url = url,
-            downloaded = format(lubridate::now("UTC"), "%Y-%m-%dT%H:%M:%SZ"),
-            sha256 = digest::digest(file = to.path, algo = "sha256"))
-        # I use the temporary variable `json` to ensure that the
-        # write only starts (and the old file is clobbered) after
-        # we've succesfully generated the new JSON.
-        json = jsonlite::toJSON(meta[order(names(meta))],
-            pretty = T, auto_unbox = T, digits = NA)
-        write(file = meta.path, json)}
+            time_downloaded = as.integer(lubridate::now("UTC")),
+            sha256 = list(digest::digest(file = to.path,
+                algo = "sha256", raw = T))))}
 
     to.path}
+
+append.one.row.to.db = function(db, table.name, l)
+  # We can't just use `dbAppendTable` because one of the columns
+  # has to be a list, and data frames can't have lists as columns.
+    dbExecute(db,
+        sqlAppendTableTemplate(db, table.name, row.names = F,
+            `colnames<-`(data.frame(t(rep(1, length(l)))), names(l))),
+        unname(l))
