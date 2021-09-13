33293347e99eff7de541fb4f2dae1dbcc602034b
Kodi Arfer <git@arfer.net>
2021-09-13 12:04:02 -0400

    Added `crs.R`.

---
+++ R/crs.R
@@ -0,0 +1,25 @@
+# Helpers for geographic coordinate reference systems (CRSes)
+
+#' @export
+crs.lonlat = 4326 # https://epsg.io/4326
+
+#' @export
+crs.us.atlas = 2163 # https://epsg.io/2163
+
+#' @export
+convert.crs = function(d, from, to, sf = F)
+  # Given a 2-column data frame or matrix of points in the CRS `from`,
+  # return a data table with columns "x" and "y" in the CRS `to`, or
+  # an `sf` object if `sf` is true.
+   {assert(length(colnames(d)) == 2)
+    orig = as.data.table(d)
+    setnames(orig, c("x", "y"))
+    orig[, by = .(x, y), index := .GRP]
+    result = sf::st_transform(crs = to, sf::st_as_sf(
+        unique(orig[, .(x, y)]),
+        coords = c("x", "y"), crs = from))
+    if (sf)
+        result[orig$index,]
+    else
+        as.data.table(`colnames<-`(value = c("x", "y"),
+            sf::st_coordinates(result)))[orig$index]}
