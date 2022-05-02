579d71d0fcb5048d4a2a5f1f870f68b9a2196aa5
Kodi Arfer <git@arfer.net>
2022-05-02 15:36:50 -0400

    [download] Added an option `decompress`.

--- R/download.R
+++ R/download.R
@@ -1,7 +1,8 @@
 #' @export
 #' @import DBI
 download.update.meta = function(
-        from, dir, to = basename(from), curl = character())
+        from, dir, to = basename(from), curl = character(),
+        decompress = NULL)
   # Download a file from the URL `from` and save it to `to.path` =
   # `dir`/`to` if there isn't already a file there, creating any
   # needed directories. Then create or update a database of metadata
@@ -11,16 +12,32 @@ download.update.meta = function(
   # If `from` is a function, it's called to download the file instead
   # of invoking `curl`. It's given one argument, `to.path`. It should
   # return a true value to indicate success.
+  #
+  # If `decompress` is set to a string such as "gz", the file is
+  # decompressed before being hashed. Generally `to` should not
+  # already have a compression suffix such as ".gz"; this will be
+  # temporarily added instead.
    {to.path = file.path(dir, to)
     dir.create(dirname(to.path), showWarnings = F, recursive = T)
+    decompression.program = if (!is.null(decompress)) switch(decompress,
+        gz = "gunzip",
+        bz2 = "bunzip2",
+        xz = "unxz",
+        stop(paste0("Unknown compression format: ", decompress)))
 
     if (!file.exists(to.path))
-       {stopifnot(if (is.function(from)) from(to.path) else 0 ==
+       {temp.to.path = paste0(to.path,
+             if (!is.null(decompress)) paste0(".", decompress))
+        stopifnot(if (is.function(from)) from(to.path) else 0 ==
             system2("curl", shQuote(c(
-                from, "-o", to.path,
+                from, "-o", temp.to.path,
                 "--fail", "--remote-time",
                 "--user-agent", "some-program",
                 curl))))
+
+        if (!is.null(decompress))
+            stopifnot(0 == system2(decompression.program,
+                shQuote(temp.to.path)))
 
         meta = dbConnect(RSQLite::SQLite(), file.path(dir, "meta.sqlite"))
         on.exit(dbDisconnect(meta))
